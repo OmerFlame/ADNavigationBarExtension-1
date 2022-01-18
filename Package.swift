2af7879570f8310cb282a35ba42fa225826a7149
OmerFlame <renginald.bagel@gmail.com>
2022-01-18 10:41:45 +0200

    Fix platform support inconsistency

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
 	name: "ADNavigationBarExtension",
+	platforms: [
+		.iOS(.v11)
+	],
 	products: [
 		.library(
 			name: "ADNavigationBarExtension",
