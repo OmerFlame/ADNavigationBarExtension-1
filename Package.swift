d34fc08dbeef51ef2f0930dab0a9c1958f239322
OmerFlame <renginald.bagel@gmail.com>
2022-01-18 10:22:48 +0200

    Dynamically convert the library to be compatible with SPM
    This is probably not going to work on the first try, so whatever.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+	name: "ADNavigationBarExtension",
+	products: [
+		.library(
+			name: "ADNavigationBarExtension",
+			targets: ["ADNavigationBarExtension"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/faberNovel/ADUtils.git", from: "v11.2.0")
+	],
+	targets: [
+		.target(
+			name: "ADNavigationBarExtension",
+			dependencies: ["ADUtils"],
+			path: "",
+			sources: ["./NavigationBarExtension/Classes"]
+		)
+	]
+)
